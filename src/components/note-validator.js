eb54458b6f756a8c4adaaeba636de38ef4b4f20d
Michael Anokye <michaelanokyej@yahoo.com>
2019-12-21 06:16:24 -0500

    updated note validator line 8

--- src/components/note-validator.js
+++ src/components/note-validator.js
@@ -5,7 +5,7 @@ const NO_ERRORS = null
 
 function getNoteValidationError({ content, folder_id, note_name }) {
   if (folder_id &&
-    (!Number.isInteger(folder_id) || folder_id < 0 || folder_id > 5)) {
+    (!Number.isInteger(folder_id) || folder_id < 0 )) {
     logger.error(`Invalid folder_id '${folder_id}' supplied`)
     return {
       error: {
